c5656cf680fe4338056ce2f039b3ac58bd94650f
Ahmad Al Attrash <ahmad.attrash@gmail.com>
2024-10-22 10:15:42 -0500

    changed syntax
    "type" to "types"

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList=[ {name:"Evee",height:5,type:"normal"},{name:"Jolteon", height:6, type:"electric"},{name:"Vaporeon", height:7,type:"water"}];
+let pokemonList = [
+  { name: "Evee", height: 5, types: "normal" },
+  { name: "Jolteon", height: 6, types: "electric" },
+  { name: "Vaporeon", height: 7, types: "water" },
+];
