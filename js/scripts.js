24865d8ddcae17bbdcb5b28176ebdb8faa13061b
Ahmad Al Attrash <ahmad.attrash@gmail.com>
2024-10-21 19:51:15 -0500

    js file updated to pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,1 @@
-let greeting = "hello world";
-alert(greeting);
-
-let favoriteFood = "lamb chops";
-document.write(favoriteFood);
+let pokemonList=[ {name:"Evee",height:5,type:"normal"},{name:"Jolteon", height:6, type:"electric"},{name:"Vaporeon", height:7,type:"water"}];
