4b2943af3981a3e7f1a342d242d131ca2c154597
Ahmad Al Attrash <ahmad.attrash@gmail.com>
2024-10-28 15:42:27 -0500

    added loop and if statement
    added the loop to go over the objects in variable and one conditional statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 let pokemonList = [
-  { name: 'Evee', height: 5, types: ['normal'] },
-  { name: 'Jolteon', height: 6, types: ['electric'] },
-  { name: 'Vaporeon', height: 7, types: ['water'] },
+  { name: 'Evee', height: 1, types: ['normal'] },
+  { name: 'Jolteon', height: 2.07, types: ['electric'] },
+  { name: 'Vaporeon', height: 3.03, types: ['water'] },
 ];
+for (let i = 0; i < pokemonList.length; i++){
+  let pokemon = pokemonList[i];
+  document.write(`${pokemon.name} (height: ${pokemon.height}) ${pokemon.height > 3 ? ' - Wow that\'s big!!' : ''}<br>`);
+  
+}
