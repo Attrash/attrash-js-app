67310aa44a23a358e88fdef261975b51508ac5c0
Ahmad Al Attrash <ahmad.attrash@gmail.com>
2024-11-03 14:35:32 -0600

    updated .JS
    *Added IIFF for the pokemonList
    * fixed forEach loop to call correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
-let pokemonList = [
-  { name: 'Evee', height: 1, types: ['normal'] },
-  { name: 'Jolteon', height: 2.07, types: ['electric'] },
-  { name: 'Vaporeon', height: 3.03, types: ['water'] },
-];
-pokemonList.forEach(displayPokemon);
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Evee", height: 1, types: ["normal"] },
+    { name: "Jolteon", height: 2.07, types: ["electric"] },
+    { name: "Vaporeon", height: 3.03, types: ["water"] },
+  ];
 
-  function displayPokemon(pokemon){
-  document.write(`${pokemon.name} (height: ${pokemon.height}) ${pokemon.height > 3 ? ' - Wow that\'s big!!' : ''}<br>`)
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+let allPokemon = pokemonRepository.getAll();
+allPokemon.forEach(displayPokemon);
 
+function displayPokemon(pokemon) {
+  document.write(`${pokemon.name} ${pokemon.height} ${pokemon.types} <br>`);
+}
